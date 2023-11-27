21dff67223dfa494a3de215f454824a516f441dd
Thorben Linker <thorbenlinker@gmail.com>
2023-11-27 14:53:41 +0100

    chore: add commentary to main method

--- prorunvis/src/main/java/prorunvis/Main.java
+++ prorunvis/src/main/java/prorunvis/Main.java
@@ -11,25 +11,30 @@ public class Main {
 
     public static void main(String[]args){
 
+        //check if an argument of sufficient length has been provided
         if(args.length == 0
                 || args[0].length() < 6){
             System.out.println("Invalid input");
             return;
         }
 
+        //check if the argument specifies a .java file
         if(!args[0].endsWith(".java")) {
             System.out.println("No .java file found");
             return;
         }
 
+        //generate a path and file object
         Path path = Paths.get(args[0]);
         File file = path.toFile().getAbsoluteFile();
 
+        //check if the file exists
         if(!file.exists()) {
             System.out.println("Specified file does not exist");
             return;
         }
 
+        //parse the file and save the resulting ast
         CompilationUnit cu;
         try {
             cu = StaticJavaParser.parse(file);
