7fccb9a0edef3e40aea61c7cc92d646808d679c0
Thorben Linker <thorbenlinker@gmail.com>
2023-11-27 14:47:22 +0100

    feat: implement main method for accepting a .java file as argument

--- prorunvis/src/main/java/prorunvis/Main.java
+++ prorunvis/src/main/java/prorunvis/Main.java
@@ -1,8 +1,40 @@
 package prorunvis;
 
+import com.github.javaparser.StaticJavaParser;
+import com.github.javaparser.ast.CompilationUnit;
+
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class Main {
 
     public static void main(String[]args){
 
+        if(args.length == 0
+                || args[0].length() < 6){
+            System.out.println("Invalid input");
+            return;
+        }
+
+        if(!args[0].endsWith(".java")) {
+            System.out.println("No .java file found");
+            return;
+        }
+
+        Path path = Paths.get(args[0]);
+        File file = path.toFile().getAbsoluteFile();
+
+        if(!file.exists()) {
+            System.out.println("Specified file does not exist");
+            return;
+        }
+
+        CompilationUnit cu;
+        try {
+            cu = StaticJavaParser.parse(file);
+        }catch(Exception e){
+            throw new RuntimeException(e.getMessage());
+        }
     }
 }
