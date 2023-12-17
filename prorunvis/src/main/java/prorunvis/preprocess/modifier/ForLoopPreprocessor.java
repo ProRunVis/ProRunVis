e95754597ccca20ea7ae2dc5e2824eda64ec674c
Des <frederik@hinz-web.de>
2023-12-17 12:49:27 +0100

    Add ForLoopPreprocessor.java

---
+++ prorunvis/src/main/java/prorunvis/preprocess/modifier/ForLoopPreprocessor.java
@@ -0,0 +1,32 @@
+package prorunvis.preprocess.modifier;
+
+import com.github.javaparser.StaticJavaParser;
+import com.github.javaparser.ast.NodeList;
+import com.github.javaparser.ast.stmt.BlockStmt;
+import com.github.javaparser.ast.stmt.ForStmt;
+import com.github.javaparser.ast.stmt.IfStmt;
+import com.github.javaparser.ast.stmt.Statement;
+import com.github.javaparser.ast.visitor.ModifierVisitor;
+
+public class ForLoopPreprocessor extends ModifierVisitor<Void> {
+    /**
+     * Modify if-statements, if the then and/or else statement is a single statement
+     * by replacing it with an equivalent block statement
+     */
+    @Override
+    public ForStmt visit(ForStmt stmt, Void arg){
+        super.visit(stmt, arg);
+
+        NodeList<Statement> statements;
+        if(!stmt.getBody().isBlockStmt()) {
+            statements = new NodeList<Statement>(stmt.getBody());
+        }
+        else {
+            statements = stmt.getBody().asBlockStmt().getStatements();
+        }
+        BlockStmt block = new BlockStmt().setStatements(statements);
+        stmt.setBody(block);
+
+        return stmt;
+    }
+}
