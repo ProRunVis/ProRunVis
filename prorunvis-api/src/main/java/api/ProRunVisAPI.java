abf32f4713db507632f52b6079c8741f810585d5
Thorben Linker <thorbenlinker@gmail.com>
2023-12-19 14:30:52 +0100

    feat: add init bean

--- prorunvis-api/src/main/java/api/ProRunVisAPI.java
+++ prorunvis-api/src/main/java/api/ProRunVisAPI.java
@@ -1,9 +1,12 @@
 package api;
 
 import api.upload.storage.StorageProperties;
+import api.upload.storage.StorageService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableConfigurationProperties(StorageProperties.class)
@@ -11,5 +14,13 @@ public class ProRunVisAPI {
 
     public static void main(String[] args){
         SpringApplication.run(ProRunVisAPI.class, args);
+    }
+
+    @Bean
+    CommandLineRunner init(StorageService storageService){
+        return (args) ->{
+            storageService.deleteAll();
+            storageService.init();
+        };
     }
 }
