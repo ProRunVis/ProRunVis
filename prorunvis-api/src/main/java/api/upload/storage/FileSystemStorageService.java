9522966d7434496b1e9fb5166c0ac95a4426f69a
Thorben Linker <thorbenlinker@gmail.com>
2023-12-19 11:23:00 +0100

    feat: add storage root location based on properties

--- prorunvis-api/src/main/java/api/upload/storage/FileSystemStorageService.java
+++ prorunvis-api/src/main/java/api/upload/storage/FileSystemStorageService.java
@@ -3,8 +3,21 @@ package api.upload.storage;
 import jakarta.servlet.http.Part;
 import org.springframework.stereotype.Service;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 @Service
 public class FileSystemStorageService implements StorageService{
+
+    private final Path rootLocation;
+
+    public FileSystemStorageService(StorageProperties properties){
+        if(properties.getLocation().trim().isEmpty()){
+            //TODO throw exception
+        }
+
+        this.rootLocation = Paths.get(properties.getLocation());
+    }
     @Override
     public void init() {
 
