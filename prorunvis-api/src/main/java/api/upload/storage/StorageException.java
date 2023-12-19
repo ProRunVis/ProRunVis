2ec0f2b5ee5896b7d6e6bd02664e46462b6ebb78
Thorben Linker <thorbenlinker@gmail.com>
2023-12-19 13:22:28 +0100

    feat: implement StorageException

--- prorunvis-api/src/main/java/api/upload/storage/StorageException.java
+++ prorunvis-api/src/main/java/api/upload/storage/StorageException.java
@@ -1,4 +1,12 @@
 package api.upload.storage;
 
 public class StorageException extends RuntimeException{
+
+    public StorageException(String message){
+        super(message);
+    }
+
+    public StorageException(String message, Throwable cause){
+        super(message, cause);
+    }
 }
