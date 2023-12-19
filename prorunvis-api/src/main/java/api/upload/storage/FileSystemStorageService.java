abdc572a7e4332fd0d87b4579526db87e0a479fc
Thorben Linker <thorbenlinker@gmail.com>
2023-12-19 11:01:21 +0100

    feat: add interface method declarations

--- prorunvis-api/src/main/java/api/upload/storage/FileSystemStorageService.java
+++ prorunvis-api/src/main/java/api/upload/storage/FileSystemStorageService.java
@@ -1,7 +1,22 @@
 package api.upload.storage;
 
+import jakarta.servlet.http.Part;
 import org.springframework.stereotype.Service;
 
 @Service
 public class FileSystemStorageService implements StorageService{
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void store(Part part) {  
+
+    }
+
+    @Override
+    public void deleteAll() {
+
+    }
 }
--- prorunvis-api/src/main/java/api/upload/storage/StorageService.java
+++ prorunvis-api/src/main/java/api/upload/storage/StorageService.java
@@ -1,4 +1,12 @@
 package api.upload.storage;
 
+import jakarta.servlet.http.Part;
+
 public interface StorageService {
+
+    void init();
+
+    void store(Part part);
+
+    void deleteAll();
 }
