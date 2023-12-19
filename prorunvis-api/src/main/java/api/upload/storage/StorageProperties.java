4e0d5dd14db6f68359c3c096ed9397c79fa79e8e
Thorben Linker <thorbenlinker@gmail.com>
2023-12-19 11:20:45 +0100

    feat: add storage properties

---
+++ prorunvis-api/src/main/java/api/upload/storage/StorageProperties.java
@@ -0,0 +1,17 @@
+package api.upload.storage;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties("storage")
+public class StorageProperties {
+
+    private String location = "/resources/in";
+
+    public String getLocation(){
+        return this.location;
+    }
+
+    public void setLocation(String location){
+        this.location = location;
+    }
+}
