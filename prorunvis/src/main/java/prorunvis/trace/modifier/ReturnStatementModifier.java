8133e7e64b3dc44f5c815a667618d00511e55404
Frederik Hinz <frederik@hinz-web.de>
2023-12-22 12:51:16 +0000

    Bugfix return stmt

--- prorunvis/src/main/java/prorunvis/trace/modifier/ReturnStatementModifier.java
+++ prorunvis/src/main/java/prorunvis/trace/modifier/ReturnStatementModifier.java
@@ -1,8 +1,8 @@
 package prorunvis.trace.modifier;
 
 import com.github.javaparser.StaticJavaParser;
-import com.github.javaparser.ast.stmt.ReturnStmt;
-import com.github.javaparser.ast.stmt.Statement;
+import com.github.javaparser.ast.NodeList;
+import com.github.javaparser.ast.stmt.*;
 import com.github.javaparser.ast.visitor.ModifierVisitor;
 
 import java.util.List;
@@ -13,25 +13,14 @@ public class ReturnStatementModifier extends ModifierVisitor<List<Integer>> {
      * Add a trace call to every return statement
      */
     @Override
-    public ReturnStmt visit(ReturnStmt stmt, List<Integer> arg){
+    public BlockStmt visit(ReturnStmt stmt, List<Integer> arg){
         String methodCall = "proRunVisTrace(\"$ID\");";
-
-        Statement parent;
-        //check if the Optional parentNode is present
-        if(stmt.getParentNode().isPresent()){
-            parent = (Statement) stmt.getParentNode().get();
-
-            //get all statements within parent and add the trace to the current index of stmt
-            parent.asBlockStmt().getStatements()
-                    .add(parent.asBlockStmt()
-                                    .getStatements().indexOf(stmt),
-                            StaticJavaParser.parseStatement(methodCall.replace("$ID", String.valueOf(arg.size()))));
-        }
-
-        //increase size of arg for the next trace-call
+        NodeList<Statement> statements = new NodeList<>();
+        statements.addFirst(stmt);
+        statements.addFirst(StaticJavaParser.parseStatement(methodCall.replace("$ID", String.valueOf(arg.size()))));
         arg.add(arg.size());
-
+        BlockStmt block = new BlockStmt(statements);
         super.visit(stmt, arg);
-        return stmt;
+        return block;
     }
 }
