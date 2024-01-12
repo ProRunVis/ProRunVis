f715b79f4f8ff0aa392c45b019afc4765c056d14
Thorben Linker <thorbenlinker@gmail.com>
2024-01-12 15:36:18 +0100

    chore: make scanner checkstyle compliant; add documentation

--- prorunvis/src/main/java/prorunvis/trace/process/Scanner.java
+++ prorunvis/src/main/java/prorunvis/trace/process/Scanner.java
@@ -1,35 +1,56 @@
 package prorunvis.trace.process;
 
 import java.io.*;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.*;
 
 public class Scanner {
 
+    /**
+     * The stream of tokens from the trace file.
+     */
     private final Stack<Integer> tokens;
+
+    /**
+     * The path to the trace file read by this scanner.
+     */
     private final Path pathToTrace;
 
-    public Scanner(String path){
+    /**
+     * Constructs a scanner to read a token stream of trace ids.
+     * @param path The path to the trace file.
+     */
+    public Scanner(final String path) {
         this.tokens = new Stack<>();
         this.pathToTrace = Paths.get(path);
     }
 
-    public void readFile() throws IOException{
+    /**
+     * Reads the file specified by {@link #pathToTrace} to {@link #tokens}
+     * in reverse order.
+     * @throws IOException If the file does not exist or could not be
+     *                     opened for other reasons.
+     */
+    public void readFile() throws IOException {
 
-        try{
+        try {
             File traceFile = pathToTrace.toFile();
             BufferedReader reader = new BufferedReader(new FileReader(traceFile));
             List<String> temp = new ArrayList<>(reader.lines().toList());
             Collections.reverse(temp);
             temp.forEach(x -> tokens.push(Integer.parseInt(x)));
-        } catch (FileNotFoundException e){
+        } catch (FileNotFoundException e) {
             throw new IOException("Could not read file.", e);
         }
     }
 
-    public Stack<Integer> getTokens(){
+    /**
+     * Gets the token stream.
+     * @return A Stack of type Integer containing the read trace ids
+     *         in reversed order.
+     */
+    public Stack<Integer> getTokens() {
         return this.tokens;
     }
 }
