e5a5b1def86dfcdfad298d30fa1dcb6f80edb4aa
Noah <doepfner.noah@gmail.com>
2024-01-12 15:50:22 +0100

    adjust preprocessor so that they pass the range of the original body to the newly created blockStmt

--- prorunvis/src/main/java/prorunvis/preprocess/modifier/DoLoopPreprocessor.java
+++ prorunvis/src/main/java/prorunvis/preprocess/modifier/DoLoopPreprocessor.java
@@ -20,12 +20,12 @@ public class DoLoopPreprocessor extends ModifierVisitor<Void> {
     public DoStmt visit(final DoStmt stmt, final Void arg) {
         super.visit(stmt, arg);
 
-        NodeList<Statement> statements;
         if (!stmt.getBody().isBlockStmt()) {
-            statements = new NodeList<>(stmt.getBody());
-            BlockStmt block = new BlockStmt().setStatements(statements);
+            BlockStmt block = new BlockStmt(new NodeList<>(stmt.getBody()));
+            block.setRange(stmt.getBody().getRange().get());
             stmt.setBody(block);
         }
+
         return stmt;
     }
 }
